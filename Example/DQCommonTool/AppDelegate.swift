7a25747f51742f62c32e809c1e5cd361cab6ba87
dengqi1 <dengqi1@lvshou.com>
2020-04-13 15:27:15 +0800

    DQLaunchScreen.

--- Example/DQCommonTool/AppDelegate.swift
+++ Example/DQCommonTool/AppDelegate.swift
@@ -14,11 +14,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     var window: UIWindow?
     
-    
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
-        
     let launchScreen = DQLaunchScreen()
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
